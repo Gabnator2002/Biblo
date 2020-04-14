ce22ec559ed22b79b7bf23f474770e20dddfd9b8
Gabriel ! <60609130+Gabnator2002@users.noreply.github.com>
2020-04-15 01:14:44 +0200

    ecf.finis*-*
    898

---
+++ monjs.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+    $("#ajouterartist").click(function(){
+        
+      window.open("ajouterartist.html","_self")
+    });
+  });
+
+  $(document).ready(function(){
+    $("#ajouterprix").click(function(){
+      window.open("ajouterprix.html","_self")
+    });
+  });
+
+  $(document).ready(function(){
+    $("#ajoutermeilleurprix").click(function(){
+      window.open("ajoutermeilleurprix.html","_self")
+    });
+  });
+
+  $(document).ready(function(){
+    $("#share").click(function(){
+      alert("partage sur facebook")
+    });
+  });
+
+  $(document).ready(function(){
+    $("#acheter").click(function(){
+        alert("va sur un site d'achat")        
+    });
+  });
+
+
+  $(document).ready(function(){
+    $("#voirfichierxml").click(function(){
+        $("#div1").load("xml.xml")        
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#enregistrerjury").click(function(){
+      window.open("jury_prem.html","_self")
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#juryloginbt").click(function(){
+      window.open("jurylogin.html","_self")
+    });
+  });
+
+  $(document).ready(function(){
+    $("#jury").click(function(){
+      window.open("jury.html","_self")
+    });
+  });
+  
+  $(document).ready(function(){
+    $(".cancelbtn").click(function(){
+      window.open("index.html","_self")
+    });
+  });
+
+
+  $(document).ready(function(){
+    $("#enregistrerauteur").click(function(){
+      window.open("auteur_prem.html","_self")
+    });
+  });
+
+  $(document).ready(function(){
+    $("#auteurloginbt").click(function(){
+      window.open("auteurlogin.html","_self")
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#auteur").click(function(){
+      window.open("auteur.html","_self")
+    });
+  });
+  
