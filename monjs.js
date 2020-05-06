f52097bb52aed09dd04c04d5e6b03c1efe05e804
Gabriel ! <60609130+Gabnator2002@users.noreply.github.com>
2020-05-06 08:20:39 +0200

    v2
    v2

--- monjs.js
+++ monjs.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
     $("#ajouterartist").click(function(){
         
-      window.open("ajouterartist.html","_self")
+      // window.open("ajouterartist.html","_self")
+      alert(" page admin pour auteur")
     });
   });
 
   $(document).ready(function(){
     $("#ajouterprix").click(function(){
-      window.open("ajouterprix.html","_self")
+      // window.open("ajouterprix.html","_self")
+      alert(" page admin pour prix")
     });
   });
 
   $(document).ready(function(){
     $("#ajoutermeilleurprix").click(function(){
-      window.open("ajoutermeilleurprix.html","_self")
+      // window.open("ajoutermeilleurprix.html","_self")
+      alert(" page admin pour meilleur prix")
     });
   });
 
@@ -36,46 +39,83 @@ $(document).ready(function(){
     });
   });
   
-  $(document).ready(function(){
-    $("#enregistrerjury").click(function(){
-      window.open("jury_prem.html","_self")
-    });
-  });
-  
-  $(document).ready(function(){
-    $("#juryloginbt").click(function(){
-      window.open("jurylogin.html","_self")
-    });
-  });
-
-  $(document).ready(function(){
-    $("#jury").click(function(){
-      window.open("jury.html","_self")
-    });
-  });
   
   $(document).ready(function(){
     $(".cancelbtn").click(function(){
       window.open("index.html","_self")
     });
   });
-
-
-  $(document).ready(function(){
-    $("#enregistrerauteur").click(function(){
-      window.open("auteur_prem.html","_self")
-    });
-  });
-
-  $(document).ready(function(){
-    $("#auteurloginbt").click(function(){
-      window.open("auteurlogin.html","_self")
-    });
-  });
   
-  $(document).ready(function(){
-    $("#auteur").click(function(){
-      window.open("auteur.html","_self")
-    });
-  });
   
+  function bonjouradmin(){
+    alert("Bonjour ADMIN")
+  }
+  function auteur_prem(){
+
+    window.open("auteur_prem.html")   
+
+  }
+  function auteur_login(){
+    // on recupere les 2 mots de passe et si il son pareil on ouvre une fenetre
+
+    const a = document.getElementById("m1").value
+    const b = document.getElementById("m2").value
+    if(a==b)   
+    { window.open("auteurlogin.html") 
+    }
+    else{alert("lse mots de passe sont pas pareil")}
+  }
+  function auteur2(){
+    window.open("auteur.html") 
+  }  
+
+
+  function jury_prem(){
+    
+    window.open("jury_prem.html")
+ 
+  }
+
+function jury_login(){
+  // on recupere les 2 mots de passe et si il son pareil on ouvre une fenetre
+  const a = document.getElementById("m3").value
+  const b = document.getElementById("m4").value
+  if(a==b)   
+  { window.open("jurylogin.html")  }
+  else{alert("lse mots de passe sont pas pareil")}  
+  
+}
+
+function jury2(){
+  window.open("jury.html") 
+}  
+
+function env_form(){
+  // ca doiut aller dans la base de donnee
+  alert('informations envoye a admin')
+}
+  
+function env_info(){
+  // ca doiut aller dans la base de donnee
+  alert('informations mise a jour')
+}
+  
+function click1(){
+  // on recupere les valeurs et on affiche
+  // ca doiut aller dans la base de donnee
+  const a = document.getElementById("d1").value
+  const b = document.getElementById("d2").value
+  const c = document.getElementById("d3").value
+  const d = document.getElementById("d4").value
+  alert('enregistré note: '+a+" "+b+" "+c+" "+d);
+}
+
+function click2(){
+  // on recupere les valeurs et on affiche
+  // ca doiut aller dans la base de donnee  
+  const a = document.getElementById("d5").value
+  const b = document.getElementById("d6").value
+  const c = document.getElementById("d7").value
+  const d = document.getElementById("d8").value
+  alert('enregistré note: '+a+" "+b+" "+c+" "+d);
+}
